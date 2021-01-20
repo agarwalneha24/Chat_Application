267b630f764027123c2e995105f842458931051c
sannu01 <53299324+sannu01@users.noreply.github.com>
2021-01-20 21:42:10 +0530

    Add files via upload

---
+++ src/components/MessageBox.js
@@ -0,0 +1,18 @@
+import React, {useRef, useEffect} from 'react'
+import Message from './Message'
+export default function MessageBox({messages}){
+  const endDiv = useRef(null)
+    useEffect(() => {
+        endDiv.current.scrollIntoView()
+    })
+  return (
+    <div className="chats">
+      {messages
+      .sort((a,b) => a.date.getTime() - b.date.getTime())
+      .map((m) => (
+        <Message message ={m} key = {m.id} />   
+      ))}
+    <div style ={{float: 'right', clear: 'both'}} ref = {endDiv}> </div>
+    </div>
+  )
+}
